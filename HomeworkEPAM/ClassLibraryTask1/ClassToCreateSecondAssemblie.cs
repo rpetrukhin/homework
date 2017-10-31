6ab7dd5941631fc9e1949d9d53db6393446107de
Mikhail Zvinchuk <mikahil.zvinchuk@yandex.ru>
2017-10-31 11:22:42 +0300

    create a multi-file assembly; build a strong name assembly;

---
+++ HomeworkEPAM/ClassLibraryTask1/ClassToCreateSecondAssemblie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTask1
+{
+    public class ClassToCreateSecondAssemblie
+    {
+        public static String VeryNiceFunction()
+        {
+            return "I'm very nice function!";
+        }
+    }
+}
