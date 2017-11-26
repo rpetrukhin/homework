6e25610e46ec5de48fe3f4880054729053ac6023
rpetrukhin <33234344+rpetrukhin@users.noreply.github.com>
2017-11-26 23:07:10 +0300

    Homework 11

---
+++ Task_11/Task_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input format is");
+            Console.WriteLine("fileName [name=someName] [lesson=someLesson] [startDate=someStartDate] " +
+                "[endDate=someEndDate] [mark=someMark] [recordsNumber=someRecordsNumber]");
+
+            string consoleString = Console.ReadLine();
+
+            var studentFilter = new StudentFilter();
+
+            var students = studentFilter.Filter(consoleString);
+
+            foreach (var student in students)
+                Console.WriteLine(student);
+        }
+    }
+}
